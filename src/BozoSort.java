cb7d24ba5fe16832221b5b4f7090c175a4130e8b
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2019-11-06 20:40:32 -0600

    changed isSorted to a private method

--- src/BozoSort.java
+++ src/BozoSort.java
@@ -12,7 +12,7 @@ public class BozoSort {
         }
     }
 
-    public static boolean isSorted(Value[] arr){
+    private static boolean isSorted(Value[] arr){
         for(int i = 1; i < arr.length; i++){
             if(arr[i].compareTo(arr[i-1]) < 0){
                 return  false;
